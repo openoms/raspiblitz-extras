4ca9b9ef1e6bb0506948e8f68a2b6836be7c7284
openoms <43343391+openoms@users.noreply.github.com>
2019-12-11 16:20:43 +0000

    Update bonus.loop.sh

--- config.scripts/bonus.loop.sh
+++ config.scripts/bonus.loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./go.install.sh
+/home/admin/config.scripts/go.install.sh
 
 source /home/admin/raspiblitz.info
 sudo cp -rf /mnt/hdd/lnd/data/chain/${network}/${chain}net /home/admin/.lnd/data/chain/${network}/
