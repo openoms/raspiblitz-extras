598ed1d81cfdb21c9706116bcee8cc48a807daaa
openoms <43343391+openoms@users.noreply.github.com>
2019-12-11 16:27:02 +0000

    Update bonus.loop.sh

--- config.scripts/bonus.loop.sh
+++ config.scripts/bonus.loop.sh
@@ -2,6 +2,9 @@
 
 /home/admin/config.scripts/go.install.sh
 
+# for paths 
+source /etc/profile
+
 source /home/admin/raspiblitz.info
 sudo cp -rf /mnt/hdd/lnd/data/chain/${network}/${chain}net /home/admin/.lnd/data/chain/${network}/
 sudo chown admin:admin /home/admin/.lnd/data/chain/${network}/${chain}net/*.macaroon
